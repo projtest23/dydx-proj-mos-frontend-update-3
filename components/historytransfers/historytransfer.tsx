8cd591f68544815f4b0e8fc447563e01218416f8
projtest23 <149867089+projtest23@users.noreply.github.com>
2025-02-19 16:36:09 +0330

    Update historytransfer.tsx

--- components/historytransfers/historytransfer.tsx
+++ components/historytransfers/historytransfer.tsx
@@ -58,7 +58,7 @@ const humanize = require('humanize-number')
       fetchData()
 
 // Fetch data every 2 seconds
-      const intervalId = setInterval(fetchData, 3000);
+      const intervalId = setInterval(fetchData, 30000);
 
 // Cleanup the interval on component unmount
       return () => clearInterval(intervalId);
